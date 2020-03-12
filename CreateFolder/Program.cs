17a087ef2d19e03e700e0ba777cc107de58defef
seba300 <sebastiandomagala98@gmail.com>
2020-03-12 10:32:53 +0100

    sec commit

---
+++ CreateFolder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CreateFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create folder
+
+            string path = @"C:\Users\Sebastian\Desktop\NewFolder";
+            System.IO.Directory.CreateDirectory(path);
+        }
+    }
+}
